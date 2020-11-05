aecea548588ef14228bfbd935792d03a91bfd57e
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2020-11-04 23:19:02 -0600

    Se agregaron modificaciones y nuevos archivos.

--- principal.php
+++ principal.php
@@ -33,14 +33,49 @@ if (!isset($usuario)) {
   <body>
     <?php include 'main/navbar.php'; ?> <!--Se manda a llamar el Navbar-->
       <div class="container">
-        <h4 class="text-center py-3">Sistema Gestor de Base de Datos</h4>
-        <h5 class="text-center py-2">Contenido</h5>
-        <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.</p>
+        <h4 class="text-center py-3">Módulos Interactivos</h4>
+          <div class="container py-3">
+            <div class="row" data-aos="fade-down">
+          <!-- inicia nuestra tarjeta de sevicios -->
+              <div class="col aling-middle">
+                <div class="card shadow p-3 mb-5 bg-white rounded" style="width: 20rem;">
+                    <div class="card-header text-center" style="background: #cc3333; color: #fff;">
+                      Recorrido Virtual
+                    </div>
+                    <a href="#"><img src="img/recVirtual.png" class="card-img-top servicios-img" alt="Modulo1"></a>
+                    <div class="card-body">
+                      <p class="card-text text-justify">Vive la experiencia como si estuvieras ahí.</p>
+                   </div>
+              </div>
+            </div><!-- termina primera card-->
+            <!-- inicia nuestra tarjeta de sevicios -->
+              <div class="col aling-middle">
+                <div class="card shadow p-3 mb-5 bg-white rounded" style="width: 20rem;">
+                  <div class="card-header text-center" style="background: #cc3333; color: #fff;">
+                    Juegos Interactivos
+                  </div>
+                    <a href="#"><img src="img/juego.jpg" class="card-img-top servicios-img" alt="Modulo2"></a>
+                  <div class="card-body">
+                     <p class="card-text text-justify">Diviertete en los juegos que tenemos para ti.</p>
+                 </div>
+              </div>
+            </div><!-- termina segunda card-->
+        <!-- inicia nuestra tarjeta de sevicios -->
+            <div class="col aling-middle">
+              <div class="card shadow p-3 mb-5 bg-white rounded" style="width: 20rem;">
+                <div class="card-header text-center" style="background: #cc3333; color: #fff;">
+                    Control de DIES
+                </div>
+                  <a href="#"><img src="img/controldies.jpg" class="card-img-top servicios-img" alt="Modulo3"></a>
+                  <div class="card-body">
+                    <p class="card-text text-justify">Mantén tu asistencia a las sesiones de manera segura.</p>
+                  </div>
+              </div>
+            <!-- termina Tercera card-->
+          </div>
+        </div>
+      </div>
+  <!-- termina seccion modulos-->
       </div>
     <script src="js/bootstrap.min.js"></script>
   </body>
